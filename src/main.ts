ae685b176a3c3c6e5407e654ed5f4a879053040e
Andrew Moore <andrew.moore21@gmail.com>
2023-02-07 22:28:50 -0500

    update

--- src/main.ts
+++ src/main.ts
@@ -5,5 +5,5 @@
  * @author Andrew Moore <amoore@mutesol.com>
  * @version 0.1.0
  */
-import Crypt, { configHMAC, configPbkdf2Sync } from './crypt/crypt'
-export { Crypt, configHMAC, configPbkdf2Sync }
+import Crypt, { configHMAC, configPbkdf2Sync, configDiffieHellman } from './crypt/crypt'
+export { Crypt, configHMAC, configPbkdf2Sync, configDiffieHellman }
